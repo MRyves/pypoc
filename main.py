e7e4f3205cc4ac7840b175876c5e6d764bcdaa00
romanwixinger <roman.wixinger@gmail.com>
2019-10-27 21:06:42 +0100

    Nikola created a working delete function

--- main.py
+++ main.py
@@ -5,13 +5,38 @@ from db_setup import dbSetup
 from db_print import dbPrint
 from db_save import dbSave
 
+from user.UserDao import UserDao, User
+from planet.PlanetDao import PlanetDao, Planet
+
+
 # 2 - database manipulation
 
 #Initialise the database up from Planet_List.xlsx
-#dbSetup("Excel_DB/DB_Setup.xlsx")
+#dbSetup()
 
 #Print all planets and moons
 #dbPrint()
 
-#Save the database to an excel file
-#dbSave()
+#delete one planet in db
+planetDao = PlanetDao()
+#planet1 = planetDao.get(10)[0]
+
+#print(f'Planet {planet1.id}: {planet1.name}')
+
+#planetDao.delete1(4)
+#planetDao.delete_all()
+erde = planetDao.get(4)
+print(erde[0].moons[0].id)
+
+#funktioniert nicht weil monde gelöscht werden
+planetDao.delete1(erde[0].moons[0].id)
+
+
+UserDao = UserDao()
+
+
+users = UserDao.getAll()
+
+for user in users:
+        print(f'User {user.id}: {user.name}')
+
--- planet/PlanetDao.py
+++ planet/PlanetDao.py
@@ -19,10 +19,26 @@ class PlanetDao:
         self.__session.add(planet)
         self.__session.commit()
 
+    # delete a planet by giving a planet as argument
+    def delete(self, planet):
+        self.__session.delete(planet)
+        self.__session.commit()
+
+    # delete a planet by giving an id as argument
+    def delete1(self, planet_id):
+        planet1 = self.get(planet_id)
+        self.delete(planet1[0])
+
+    #funktioniert noch nicht, wenn planet monde hat
+    def delete_all(self):
+        planets = PlanetDao.getAll(self)
+        for planet in planets:
+            self.delete1(planet.id)
+
     def getAll(self) -> List[Planet]:
         return self.__session.query(Planet).all()
 
-    def get(self, planetId):
+    def get(self, planetId) -> List[Planet]:
         """
 
         :type planetId: int
